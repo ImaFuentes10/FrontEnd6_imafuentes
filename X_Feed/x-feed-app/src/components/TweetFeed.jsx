694eed38e44a5a1c0a3db6e60e673b8230e75050
ImaFuentes10 <ima.fuentes.c@gmail.com>
2025-06-24 18:46:37 -0700

    feat: :sparkles: Second part styles TweetCard
    Added icons, finished outline
    
    Missing pp, effects, multimedia container

--- X_Feed/x-feed-app/src/components/TweetFeed.jsx
+++ X_Feed/x-feed-app/src/components/TweetFeed.jsx
@@ -9,7 +9,7 @@ function TweeetFeed () {
         <>
             <div className="bg-black w-screen h-screen flex">
                 <SideBar />
-                <div className="flex flex-col w-128 border-x border-zinc-900">
+                <div className="flex flex-col w-128 border-x border-zinc-900 flex-wrap">
                     <TweetComposer />
                     <TweetCard name="Juan Gabriel" userName="juanga" date="22 jun" />
                 </div>
