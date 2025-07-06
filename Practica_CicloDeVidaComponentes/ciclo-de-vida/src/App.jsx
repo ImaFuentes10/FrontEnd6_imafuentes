42e66211bd165991a64a1695a64ab503f7545144
ImaFuentes10 <ima.fuentes.c@gmail.com>
2025-07-06 09:50:59 -0600

    feat: :sparkles: Fixed control panel message and added warnings
    Fixed status of control panel, added warning messages for low fuel, stoppes fuel interval when 0 and changed interval for ditance after this
    
    interval of fuel now stops at 0

--- Practica_CicloDeVidaComponentes/ciclo-de-vida/src/App.jsx
+++ Practica_CicloDeVidaComponentes/ciclo-de-vida/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect, useMemo, useRef } from 'react'
 import './App.css'
-import Planet from './components/Planeta';
+/* import Planet from './components/Planeta'; */
 
 function App() {
   const [distance, setDistance ] = useState(0);
-  const [fuel, setFuel] = useState(100);
-  const [shipStatus, setShipStatus] = useState("");
-  const [visitedPlanets, setVisitedPlanets] = useState([]);
-  /* const [controlPanel, setControlPanel] = useState(""); */
-  const [buttonText, setButtonText] = useState("")
+  const [fuel, setFuel] = useState(20);
+  const [shipStatus, setShipStatus] = useState("En órbita");
+  /* const [visitedPlanets, setVisitedPlanets] = useState([]); */
+  const [controlPanel, setControlPanel] = useState("");
+  const [buttonText, setButtonText] = useState("Aterrizar")
+  const [warning, setWarning] = useState("")
+  const prevStatus = useRef();
 
+  //useEffect para llevar intervalo de combustible y distancai principal
   useEffect(() => {
-    console.log("¡El panel de control está listo!");
-    setShipStatus("En órbita");
-    setButtonText("Aterrizar")
-
-    const interval = setInterval(() => {
-      setFuel(f => f - 1);
-      setDistance(d => d + 1);
-    }, 1000);
-
-    return () => {
-      clearInterval(interval)
-      console.log("El panel de control se ha apagado")
+    let interval;
+    if (
+      (shipStatus === "En órbita" || 
+      shipStatus === "Aterrizando" || 
+      shipStatus === "Despegando") 
+      && fuel > 0
+    ) {
+      interval = setInterval(() => {
+        setFuel(f => f - 1);
+        setDistance(d => d + 1);
+      }, 1000);
     }
-  },[])
 
+    return () => clearInterval(interval);
+  }, [shipStatus, fuel]);
+
+  //useEffect para mensaje de panel de control al haber aterrizado
+  useEffect(() => {
+    if (shipStatus === "Aterrizado")
+      setControlPanel("El panel de control se ha apagado");
+  }, [shipStatus]);
+
+  //useEffect para mensaje de panel de control al estar enórbita o despegar
+  useEffect(() => {
+    if (
+      shipStatus === "Despegando" && prevStatus.current === "Aterrizado" 
+      || prevStatus.current === undefined)
+    {
+      setControlPanel("¡El panel de control está listo!");
+    }
+    prevStatus.current = shipStatus;
+  }, [shipStatus]);
+
+  //memoria del estado de la nave
   const statusMessage = useMemo(() => {
     return `Estado: ${shipStatus}`
   }, [shipStatus])
 
+  //cambio de los estados de la nave
   const changeStatus = () => {
     if (shipStatus === "En órbita") {
       setShipStatus("Aterrizando");
@@ -48,23 +71,54 @@ function App() {
     } 
   }
 
+  //useEffect para mensajes de alerta por combustible bajo
+  useEffect(() => {
+    if (fuel <= 0) {
+      setWarning("¡SIN COMBUSTIBLE! A LA DERIVA...");
+    } else if (fuel <= 5) {
+      setWarning("¡PELIGRO!");
+    } else if (fuel <= 10){
+      setWarning("¡POCO COMBUSTIBLE!");
+    }
+  }, [fuel]);
+  
+  //useMemo para mensaje de alerta
+  const warningMessage = useMemo(() => {
+    return `${warning}`
+  }, [warning])
 
+  //useEffect para cambio de distancia por movimiento a la deriva
+  useEffect(() => {
+    let interval;
+    if (
+      (shipStatus === "En órbita" || 
+      shipStatus === "Aterrizando" || 
+      shipStatus === "Despegando") 
+      && fuel == 0
+    ) {
+      interval = setInterval(() => {
+        setDistance(d => d + 1);
+      }, 4000);
+    }
 
+    return () => clearInterval(interval);
+  }, [shipStatus, fuel]);
+  
   return (
     <>
       <div id='controlPanel'>
-        {/* <p>{controlPanel}</p> */}
+        <p>{warningMessage}</p>
+        <p>{controlPanel}</p>
         <p>Combustible: {fuel}</p>
         <p>Distancia: {distance}</p>
         <p>{statusMessage}</p>
         <button onClick={changeStatus}>{buttonText}</button>
       </div>
-      <div>
+      {/* <div>
         {visitedPlanets.map((Planet, index) => ( 
           <Planet key={index} name={Planet} />
         ))}
-      </div>
-      
+      </div> */}
     </>
   )
 }
